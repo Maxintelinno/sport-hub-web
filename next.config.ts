2453ab0e1c69a971f796bda76dc0ca7de17e7c71
Osacn thakrit Ratchakote <osacn_thakrit.ratchakote@krungthai.com>
2026-04-24 00:56:34 +0700

    fix: deployment config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.18', 'localhost:3000']
+  experimental: {
+    allowedDevOrigins: ['192.168.1.18', 'localhost:3000']
+  }
 };
 
 export default withNextIntl(nextConfig);
